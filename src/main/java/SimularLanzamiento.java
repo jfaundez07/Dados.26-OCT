05daace4a9519bab2dfee95933dac85c33598561
jfaundez07 <j.faundez07@ufromail.cl>
2023-10-29 17:11:09 -0300

    Clase SimularLanzamiento

--- src/main/java/SimularLanzamiento.java
+++ src/main/java/SimularLanzamiento.java
@@ -4,8 +4,31 @@ public class SimularLanzamiento {
 	private Dado dado2;
 
 	public void jugar() {
-		// TODO - implement SimularLanzamiento.jugar
-		throw new UnsupportedOperationException();
+
+		dado1 = new Dado();
+		dado2 = new Dado();
+
+		int lanzarDado1 = dado1.lanzarDado();
+		int lanzarDado2 = dado2.lanzarDado();
+
+		Calculadora calculadora = new Calculadora(lanzarDado1, lanzarDado2);
+
+		System.out.println("\n---> Lanzamiento dado1: " + lanzarDado1);
+		System.out.println("---> Lanzamiento dado2: " + lanzarDado2);
+
+		realizarJuego(calculadora);
+	}
+
+	public boolean validarGanar(int num) {
+		return num == 7;
+	}
+
+	public void realizarJuego(Calculadora calculadora) {
+		if (validarGanar(calculadora.sumar())) {
+			System.out.println("\n#Ganaste!");
+		} else {
+			System.out.println("\n#Perdiste!");
+		}
 	}
 
 }
